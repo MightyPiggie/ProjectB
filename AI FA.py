e43dc3f71b15e5ab546d8b630f007c44d6e64ef7
MightyPiggie <degamendekoe@gmail.com>
2020-11-24 11:44:11 +0100

    Add files via upload

--- AI FA.py
+++ AI FA.py
@@ -1,7 +1,7 @@
 import json
+def open_file():
+    with open('steam.json') as json_file:
+        data = json.load(json_file)
+        print(data[0]['appid'])
 
-with open('steam.json') as json_file:
-    data = json.load(json_file)
-
-
-    print(data[0]['appid'])
+open_file()
